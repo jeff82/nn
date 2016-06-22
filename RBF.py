8d546d33a2529e54acaf4465346d530f521d52f6
jeff82 <liuzhen@tetelaser.com.cn>
2016-06-22 18:59:11 +0800

    -m

--- RBF.py
+++ RBF.py
@@ -20,9 +20,9 @@ class RBF:
         self.allE=[]
 
     def _basisfunc(self,c, d,cid):
-        print c,"ccc"
-        print d,"xxx",c-d
-        print exp(-0.5/(self.beta[cid]*self.beta[cid]) * norm(c.T-d.T)**2),norm(c-d)**2,norm(c.T-d.T)**2,len(d),self.indim
+#        print c,"ccc"
+#        print d,"xxx",c-d
+      #  print exp(-0.5/(self.beta[cid]*self.beta[cid]) * norm(c.T-d.T)**2),norm(c-d)**2,norm(c.T-d.T)**2,len(d),self.indim
         assert len(d) == self.indim
         return exp(-0.5/(self.beta[cid]*self.beta[cid]) * norm(c-d)**2)
     def _calcAct(self, X):
@@ -40,7 +40,7 @@ class RBF:
         #print "center", self.centers
     # calculate activations of RBFs
         G = self._calcAct(X)
-        print len(G)  # calculate output weights (pseudoinverse)
+        print "gg",G.shape  # calculate output weights (pseudoinverse)
         self.W = dot(pinv(G), Y)
         #self.ERR=zeros((X.shape[0], self.outdim), float)
         self.allE =zeros((1,self.outdim),float)
@@ -58,28 +58,59 @@ class RBF:
         delta_Weight    = zeros((self.indim,self.numCenters),float)
 
         sum1=sum2=sum3=0
-        print "dddd"
-        matCntr=numpy.ndarray.tolist(self.centers)
-        print self.centers
-        matCntr=array(matCntr*G.shape[0]).reshape(G.shape[0],self.numCenters)
-        print matCntr
-        print x
-        Dist=x-matCntr
+        matCntr=[]
         s3=(self.ERR*G)
-        s1=s3*Dist
-        s2=s1*Dist        
-        print s1
+        for id, xi in enumerate(x):
+            matCntr=xi-self.centers
+            mat2=matCntr*matCntr
+            
+            print "cc",matCntr
+       
+       
+       
+#       print "dddd"
+#        matCntr=numpy.ndarray.tolist(self.centers.T)
+#        print self.centers
+#        matCntr=array(array([matCntr]*G.shape[0]).T).reshape(self.numCenters,G.shape[0],self.indim)
+#        print "mmmm",G
+#        print matCntr,matCntr.shape
+#        print "xxx"
+#        print x,x.shape
+#        Dist=x-matCntr
+#        print Dist
+#        s3=(self.ERR*G)
+#        print s3 
+#        print s3.shape
+#        s1=s2=zeros((self.indim,G.shape[0],self.numCenters),float)
+#        print Dist.shape
+#        for i,dist in enumerate(Dist.T):
+#            print i
+#            s1[i]=s3*dist
+#            s2[i]=s3*dist*dist   
+#            
+#        #print s1,s2
+#        print s1.shape,s2.shape,s3.shape,Dist.shape
+        """
         
+        """
+        
+#void updateParam(){
+#    for(int j=0;j<M;++j){m====centre num
+#        double delta_center=0.0,delta_delta=0.0,delta_weight=0.0;
+#        double sum1=0.0,sum2=0.0,sum3=0.0;
+#        for(int i=0;i<P;++i){p  train num
+#            sum1+=error[i]*exp(-1.0*(X[i]-center[j])*(X[i]-center[j])/(2*delta[j]*delta[j]))*(X[i]-center[j]);
+#            sum2+=error[i]*exp(-1.0*(X[i]-center[j])*(X[i]-center[j])/(2*delta[j]*delta[j]))*(X[i]-center[j])*(X[i]-center[j]);
+#            sum3+=error[i]*exp(-1.0*(X[i]-center[j])*(X[i]-center[j])/(2*delta[j]*delta[j]));
+#        }
 #        delta_center=eta*Weight[j]/(delta[j]*delta[j])*sum1;
 #        delta_delta=eta*Weight[j]/pow(delta[j],3)*sum2;
 #        delta_weight=eta*sum3;
 #        center[j]+=delta_center;
 #        delta[j]+=delta_delta;
 #        Weight[j]+=delta_weight;
-#             sum1+=self.ERR[i]*exp(-1.0*(X[i]-center[j])*(X[i]-center[j])/(2*delta[j]*delta[j]))*(X[i]-center[j]);
-#             sum2+=self.ERR[i]*exp(-1.0*(X[i]-center[j])*(X[i]-center[j])/(2*delta[j]*delta[j]))*(X[i]-center[j])*(X[i]-center[j]);
-#             sum3+=self.ERR[i]*exp(-1.0*(X[i]-center[j])*(X[i]-center[j])/(2*delta[j]*delta[j]));
-        
+#    }
+#}
 
     def test(self, X):
         """ X: matrix of dimensions n x indim """
@@ -92,16 +123,18 @@ if __name__ == '__main__':
     n = 4
     ncenters=3
     indim=2
+    outdim=1
 
-    x = mgrid[-1:1:complex(0,n)].reshape(n, 1)
-    x=numpy.ndarray.tolist(x)*indim
-    x=array(x).reshape(indim,n).T
+    x = mgrid[-1:1:complex(0,2*n)].reshape(n, 2)
+    #x=numpy.ndarray.tolist(x)*indim
+    #x=array(x).reshape(indim,n).T
     print x
     # set y and add random noise
-    y = sin(3*(x+0.5)**3 - 1)
+    y = array(sin(3*(x[:,0]+0.5)**3 - 1)+x[:,1]).reshape(n,outdim)
+    print "yy",y
     # y += random.normal(0, 0.1, y.shape)
     #  rbf regression
-    rbf = RBF(indim, ncenters, 1)
+    rbf = RBF(indim, ncenters, outdim)
     rbf.beta=mgrid[0.2:0.2:complex(0,ncenters)].reshape(ncenters, 1)
     rnd_idx = random.permutation(x.shape[0])[:rbf.numCenters]
     rbf.centers = array([x[i,:] for i in rnd_idx])
